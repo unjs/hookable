a0d9146c40fb9767842564225444c6c5a7dce70f
Daniel Roe <daniel@roe.dev>
2021-09-01 19:05:41 +0100

    fix: type nested/namespaced hooks (#32)

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,33 @@
 export type HookCallback = (...args: any) => Promise<void> | void
 export interface Hooks { [key: string]: HookCallback }
 export type HookKeys<T> = keyof T & string
-export type NestedHooks<T> = Partial<{ [name in HookKeys<T>]: NestedHooks<T> | HookCallback }>
 export type DeprecatedHook<T> = string | { message?: string, to: HookKeys<T> }
 export type DeprecatedHooks<T> = { [name in HookKeys<T>]: DeprecatedHook<T> }
+
+// Utilities
+type ValueOf<C> = C extends Record<any, any> ? C[keyof C] : never
+type Strings<T> = Exclude<keyof T, number | symbol>
+type KnownKeys<T> = keyof {
+  [K in keyof T as string extends K ? never : number extends K ? never : K]: never
+}
+type StripGeneric<T> = Pick<T, KnownKeys<T> extends keyof T ? KnownKeys<T> : never>
+type OnlyGeneric<T> = Omit<T, KnownKeys<T> extends keyof T ? KnownKeys<T> : never>
+
+// Unwrapping utilities
+type Namespaces<T> = ValueOf<{
+  [key in Strings<T>]: key extends `${infer Namespace}:${string}` ? Namespace : never
+}>
+type BareHooks<T> = ValueOf<{
+  [key in Strings<T>]: key extends `${string}:${string}` ? never : key
+}>
+type HooksInNamespace<T, Namespace extends string> = ValueOf<{
+  [key in Strings<T>]: key extends `${Namespace}:${infer HookName}` ? HookName : never
+}>
+type WithoutNamespace<T, Namespace extends string> = {
+  [key in HooksInNamespace<T, Namespace>]: `${Namespace}:${key}` extends keyof T ? T[`${Namespace}:${key}`] : never
+}
+
+export type NestedHooks<T> =
+  (Partial<StripGeneric<T>> | Partial<OnlyGeneric<T>>) &
+  Partial<{ [key in Namespaces<StripGeneric<T>>]: NestedHooks<WithoutNamespace<T, key>> }> &
+  Partial<{ [key in BareHooks<StripGeneric<T>>]: T[key] }>
---
+++ test/types.test.ts
@@ -0,0 +1,43 @@
+import { expectTypeOf } from 'expect-type'
+import { createHooks } from '../src'
+import { HookCallback } from '../types'
+
+describe('hook types', () => {
+  test('correctly handles non-nested hooks', () => {
+    const hooks = createHooks<{ foo: () => true, bar: (_arg: string) => 42 }>()
+
+    expectTypeOf(hooks.hook).parameter(0).not.toBeAny()
+    expectTypeOf(hooks.hook).parameter(0).toEqualTypeOf<'foo' | 'bar'>()
+
+    // @ts-expect-error
+    hooks.hook('foo', _param => true)
+  })
+
+  test('handles nested hooks', () => {
+    const hooks = createHooks<{
+      'namespace:foo': (arg: number) => void
+      bar: (_arg: string) => void
+      'namespace:baz': (arg: '42') => void
+    }>()
+
+    // should be valid
+    hooks.addHooks({ namespace: { foo: (_arg) => { }, baz: (_arg) => { } }, bar: (_arg) => { }, 'namespace:foo': () => { } })
+    // @ts-expect-error
+    hooks.addHooks({ a: (_arg) => { } })
+    // @ts-expect-error
+    hooks.addHooks({ namespace: { nonexistent: (_arg) => { } } })
+  })
+
+  test('handles nested hooks with signature', () => {
+    const hooks = createHooks<{
+      [key: string]: HookCallback
+      'namespace:foo': (arg: number) => void
+      bar: (_arg: string) => void
+    }>()
+
+    // should both be valid
+    hooks.addHooks({ namespace: { foo: (_arg) => { } }, bar: (_arg) => { }, 'namespace:foo': () => { } })
+    hooks.addHooks({ namespace: { nothing: (_arg) => { } } })
+    hooks.addHooks({ nothing: (_arg) => { } })
+  })
+})
