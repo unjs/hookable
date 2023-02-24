ae0d14400d13441b1695ce99ba9c24c671b356c9
Daniel Roe <daniel@roe.dev>
2023-02-24 12:08:05 +0100

    style: lint project and apply template lint patterns

--- test/debuger.test.ts
+++ test/debuger.test.ts
@@ -1,62 +1,80 @@
-import { afterAll, beforeAll, describe, it, beforeEach, expect, vi } from 'vitest'
-import { createDebugger, Hookable } from '../src/index'
+import {
+  afterAll,
+  beforeAll,
+  describe,
+  it,
+  beforeEach,
+  expect,
+  vi,
+} from "vitest";
+import { createDebugger, Hookable } from "../src/index";
 
-const consoleMethods = ['time', 'timeEnd', 'timeLog', 'groupCollapsed', 'groupEnd'] as const
+const consoleMethods = [
+  "time",
+  "timeEnd",
+  "timeLog",
+  "groupCollapsed",
+  "groupEnd",
+] as const;
 
-describe('debugger', () => {
-  let hooks: Hookable
+describe("debugger", () => {
+  let hooks: Hookable;
 
   beforeAll(() => {
-    consoleMethods.forEach((l) => { console[l] = vi.fn() })
-  })
+    for (const l of consoleMethods) {
+      console[l] = vi.fn();
+    }
+  });
   beforeEach(() => {
-    hooks = new Hookable()
-    vi.clearAllMocks()
-  })
+    hooks = new Hookable();
+    vi.clearAllMocks();
+  });
   afterAll(() => {
-    vi.restoreAllMocks()
-  })
+    vi.restoreAllMocks();
+  });
 
-  it('should respect `tag` option', async () => {
-    createDebugger(hooks, { tag: 'tag' })
-    await hooks.callHook('hook')
-    expect(console.time).toBeCalledWith(expect.stringContaining('[tag] hook'))
-    expect(console.timeEnd).toBeCalledWith(expect.stringContaining('[tag] hook'))
-  })
-  it('should respect `inspect` option', async () => {
-    createDebugger(hooks, { inspect: true })
-    await hooks.callHook('hook')
-    expect(console.time).toBeCalledWith(expect.stringContaining('hook'))
-    expect(console.timeLog).toBeCalledWith('hook', [])
-  })
-  it('should respect `group` option', async () => {
-    createDebugger(hooks, { group: true })
-    await hooks.callHook('hook')
-    expect(console.groupCollapsed).toBeCalled()
-    expect(console.groupEnd).toBeCalled()
-  })
-  it('should respect `filter` option as string', async () => {
-    createDebugger(hooks, { filter: 'other:' })
-    await hooks.callHook('hook')
-    expect(console.time).not.toBeCalled()
-    await hooks.callHook('other:hook')
-    expect(console.time).toBeCalled()
-  })
-  it('should respect `filter` option as function', async () => {
-    createDebugger(hooks, { filter: id => id === 'other:hook' })
-    await hooks.callHook('hook')
-    expect(console.time).not.toBeCalled()
-    await hooks.callHook('other:hook')
-    expect(console.time).toBeCalled()
-  })
-  it('should allowing closing debugger', async () => {
-    const debug = createDebugger(hooks)
-    await hooks.callHook('hook')
-    expect(console.time).toBeCalled()
-    debug.close()
-    vi.clearAllMocks()
-    await hooks.callHook('hook')
-    expect(console.time).not.toBeCalled()
-    debug.close()
-  })
-})
+  it("should respect `tag` option", async () => {
+    createDebugger(hooks, { tag: "tag" });
+    await hooks.callHook("hook");
+    expect(console.time).toBeCalledWith(expect.stringContaining("[tag] hook"));
+    expect(console.timeEnd).toBeCalledWith(
+      expect.stringContaining("[tag] hook")
+    );
+  });
+  it("should respect `inspect` option", async () => {
+    createDebugger(hooks, { inspect: true });
+    await hooks.callHook("hook");
+    expect(console.time).toBeCalledWith(expect.stringContaining("hook"));
+    expect(console.timeLog).toBeCalledWith("hook", []);
+  });
+  it("should respect `group` option", async () => {
+    createDebugger(hooks, { group: true });
+    await hooks.callHook("hook");
+    expect(console.groupCollapsed).toBeCalled();
+    expect(console.groupEnd).toBeCalled();
+  });
+  it("should respect `filter` option as string", async () => {
+    createDebugger(hooks, { filter: "other:" });
+    await hooks.callHook("hook");
+    expect(console.time).not.toBeCalled();
+    await hooks.callHook("other:hook");
+    expect(console.time).toBeCalled();
+  });
+  it("should respect `filter` option as function", async () => {
+    createDebugger(hooks, { filter: (id) => id === "other:hook" });
+    await hooks.callHook("hook");
+    expect(console.time).not.toBeCalled();
+    await hooks.callHook("other:hook");
+    expect(console.time).toBeCalled();
+  });
+  it("should allowing closing debugger", async () => {
+    const debug = createDebugger(hooks);
+    await hooks.callHook("hook");
+    expect(console.time).toBeCalled();
+    debug.close();
+    vi.clearAllMocks();
+    await hooks.callHook("hook");
+    expect(console.time).not.toBeCalled();
+    debug.close();
+  });
+});
--- test/types.test.ts
+++ test/types.test.ts
@@ -1,71 +1,91 @@
-import { describe, test } from 'vitest'
-import { expectTypeOf } from 'expect-type'
-import { createHooks } from '../src'
-import type { HookCallback } from '../src/types'
+import { describe, test } from "vitest";
+import { expectTypeOf } from "expect-type";
+import { createHooks } from "../src";
+import type { HookCallback } from "../src/types";
 
-describe('hook types', () => {
-  test('correctly handles non-nested hooks', () => {
-    const hooks = createHooks<{ foo:() => true, bar: (_arg: string) => 42 }>()
+describe("hook types", () => {
+  test("correctly handles non-nested hooks", () => {
+    const hooks = createHooks<{
+      foo: () => true;
+      bar: (_argument: string) => 42;
+    }>();
 
-    expectTypeOf(hooks.hook).parameter(0).not.toBeAny()
-    expectTypeOf(hooks.hook).parameter(0).toEqualTypeOf<'foo' | 'bar'>()
+    expectTypeOf(hooks.hook).parameter(0).not.toBeAny();
+    expectTypeOf(hooks.hook).parameter(0).toEqualTypeOf<"foo" | "bar">();
 
     // @ts-expect-error
-    hooks.hook('foo', _param => true)
-  })
+    hooks.hook("foo", (_parameter) => true);
+  });
 
-  test('deprecates hooks', () => {
-    const hooks = createHooks<{ foo:() => true, bar: (_arg: string) => 42 }>()
-    hooks.deprecateHooks({
-      foo: { to: 'bar' }
-    })
-  })
-
-  test('handles nested hooks', () => {
+  test("deprecates hooks", () => {
     const hooks = createHooks<{
-      'namespace:foo':(arg: number) => void
-      bar: (_arg: string) => void
-      'namespace:baz': (arg: '42') => void
-    }>()
+      foo: () => true;
+      bar: (_argument: string) => 42;
+    }>();
+    hooks.deprecateHooks({
+      foo: { to: "bar" },
+    });
+  });
+
+  test("handles nested hooks", () => {
+    const hooks = createHooks<{
+      "namespace:foo": (argument: number) => void;
+      bar: (_argument: string) => void;
+      "namespace:baz": (argument: "42") => void;
+    }>();
 
     // should be valid
-    hooks.addHooks({ namespace: { foo: (_arg) => { }, baz: (_arg) => { } }, bar: (_arg) => { }, 'namespace:foo': () => { } })
+    hooks.addHooks({
+      namespace: { foo: (_argument) => {}, baz: (_argument) => {} },
+      bar: (_argument) => {},
+      "namespace:foo": () => {},
+    });
     // @ts-expect-error
-    hooks.addHooks({ a: (_arg) => { } })
+    hooks.addHooks({ a: (_argument) => {} });
     // @ts-expect-error
-    hooks.addHooks({ namespace: { nonexistent: (_arg) => { } } })
-  })
+    hooks.addHooks({ namespace: { nonexistent: (_argument) => {} } });
+  });
 
-  test('handles nested hooks with signature', () => {
+  test("handles nested hooks with signature", () => {
     const hooks = createHooks<{
-      [key: string]: HookCallback
-      'namespace:foo':(arg: number) => void
-      bar: (_arg: string) => void
-    }>()
+      [key: string]: HookCallback;
+      "namespace:foo": (argument: number) => void;
+      bar: (_argument: string) => void;
+    }>();
 
     // should both be valid
-    hooks.addHooks({ namespace: { foo: (_arg) => { } }, bar: (_arg) => { }, 'namespace:foo': () => { } })
-    hooks.addHooks({ namespace: { nothing: (_arg) => { } } })
-    hooks.addHooks({ nothing: (_arg) => { } })
-  })
+    hooks.addHooks({
+      namespace: { foo: (_argument) => {} },
+      bar: (_argument) => {},
+      "namespace:foo": () => {},
+    });
+    hooks.addHooks({ namespace: { nothing: (_argument) => {} } });
+    hooks.addHooks({ nothing: (_argument) => {} });
+  });
 
-  test('beforeEach and afterEach typings', () => {
-    const hooks = createHooks<{ foo:() => true, bar: (_arg: number) => 42 }>()
+  test("beforeEach and afterEach typings", () => {
+    const hooks = createHooks<{
+      foo: () => true;
+      bar: (_argument: number) => 42;
+    }>();
 
-    expectTypeOf(hooks.beforeEach).parameter(0).not.toBeAny()
-    expectTypeOf(hooks.afterEach).parameter(0).parameter(0).toEqualTypeOf<
-      { name: 'foo', args: [], context: Record<string, any> } |
-      { name: 'bar', args: [number], context: Record<string, any> }
-    >()
+    expectTypeOf(hooks.beforeEach).parameter(0).not.toBeAny();
+    expectTypeOf(hooks.afterEach)
+      .parameter(0)
+      .parameter(0)
+      .toEqualTypeOf<
+        | { name: "foo"; args: []; context: Record<string, any> }
+        | { name: "bar"; args: [number]; context: Record<string, any> }
+      >();
 
     hooks.beforeEach(({ name, args }) => {
-      expectTypeOf(name).toEqualTypeOf<'foo' | 'bar'>()
-      if (name === 'foo') {
-        expectTypeOf(args).toEqualTypeOf<[]>()
+      expectTypeOf(name).toEqualTypeOf<"foo" | "bar">();
+      if (name === "foo") {
+        expectTypeOf(args).toEqualTypeOf<[]>();
       }
-      if (name === 'bar') {
-        expectTypeOf(args[0]).toEqualTypeOf<number>()
+      if (name === "bar") {
+        expectTypeOf(args[0]).toEqualTypeOf<number>();
       }
-    })
-  })
-})
+    });
+  });
+});
