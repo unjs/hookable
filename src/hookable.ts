12153677896bc631bb2335a5b187fed6fffef273
Daniel Roe <daniel@roe.dev>
2023-03-28 19:58:20 +0100

    fix: clone hook arrays before calling

--- src/hookable.ts
+++ src/hookable.ts
@@ -218,18 +218,18 @@ export class Hookable<
         ? { name, args: arguments_, context: {} }
         : undefined;
     if (this._before) {
-      callEachWith(this._before, event);
+      callEachWith([...this._before], event);
     }
-    const result = caller(this._hooks[name] || [], arguments_);
+    const result = caller(name in this._hooks ? [...this._hooks[name]] : [], arguments_);
     if ((result as any) instanceof Promise) {
       return result.finally(() => {
         if (this._after && event) {
-          callEachWith(this._after, event);
+          callEachWith([...this._after], event);
         }
       });
     }
     if (this._after && event) {
-      callEachWith(this._after, event);
+      callEachWith([...this._after], event);
     }
     return result;
   }
