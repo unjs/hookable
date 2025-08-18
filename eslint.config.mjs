ccbe59d87d6ff77a8c91e6287e7e662211730faa
Pooya Parsa <pooya@pi0.io>
2025-08-18 14:14:24 +0200

    chore: update eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import unjs from "eslint-config-unjs";
+
+export default unjs({
+  ignores: [
+    // ignore paths
+  ],
+  rules: {
+    // rule overrides
+  },
+  markdown: {
+    rules: {
+      // markdown rule overrides
+    },
+  },
+});
