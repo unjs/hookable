269dd0299cff5cb666e1670bba954e663b47ff07
Daniel Roe <daniel@roe.dev>
2026-03-14 23:16:43 +0000

    feat: add `clearHook(name)` (#135)

--- src/hookable.ts
+++ src/hookable.ts
@@ -140,6 +140,10 @@ export class Hookable<
     }
   }
 
+  clearHook<NameT extends HookNameT>(name: NameT): void {
+    this._hooks[name] = undefined;
+  }
+
   deprecateHook<NameT extends HookNameT>(
     name: NameT,
     deprecated: HookKeys<HooksT> | DeprecatedHook<HooksT>,
